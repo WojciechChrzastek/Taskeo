d53a28d2c6af49037b527d783aa4e34b986fddf8
WojciechChrzastek <w.chrzastek@gmail.com>
2018-03-14 09:41:15 +0100

    Change Spring Boot to 1.5.10.RELEASE

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,11 +1,18 @@
 package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
+
+    @Modifying
+    void deleteById(Long id);
+
+    Optional<Task> findById(Long id);
 }
